eab167507a5b720b5e0887a3a0cddf70fc39422c
unknown <eloso85@gmail.com>
2021-11-28 20:57:43 -0600

    first commit

---
+++ web_3_simple_storage/deploy.py
@@ -0,0 +1,42 @@
+import json
+
+from solcx import compile_standard, install_solc
+
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+    install_solc("0.6.0")
+
+    compiled_sol = compile_standard(
+        {
+            "language": "Solidity",
+            "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+            "settings": {
+                "outputSelection": {
+                    "*": {
+                        "*": [
+                            "abi",
+                            "metadata",
+                            "evm.bytecode",
+                            "evm.bytecode.sourceMap",
+                        ]
+                    }
+                }
+            },
+        },
+        solc_version="0.6.0",
+    )
+
+    with open("compiled_code.json", "w") as file:
+        json.dump(compiled_sol, file)
+
+    # get bytecode to deploy  this is walking down the compiled_code json contracts/simpleStorage.sol/simplestorage/evm
+
+    bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+        "bytecode"
+    ]["object"]
+
+    # get abi
+
+    abi = json.loads(
+        compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"]
+    )["output"]
